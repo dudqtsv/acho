910a7295d9a6e568e20c3712f9826967fec1b7d3
maria <dudacarrijo36@gmail.com>
2026-04-23 14:31:23 -0300

    pipipipopopo

---
+++ conexao.php
@@ -0,0 +1,14 @@
+<?php
+
+    $host = "mysql_lab";
+    $user = "root";
+    $password = "123";
+    $bd = "banco";
+    $conexao = new mysql($host, $user, $password, $bd);
+
+    if($conexao->connect_error){
+        die("Erro na conexão" . $conexao->connect_error);
+    }
+    echo "Conectado com sucesso"
+
+?>
